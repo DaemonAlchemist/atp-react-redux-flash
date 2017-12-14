c916cd9c345b63d3212e90143c0089e5afdfe2c2
Andrew Wittrock <andy@wittrock.us>
2017-12-14 10:02:52 -0600

    Fix flash message positioning

--- src/components/flash-messages.jsx
+++ src/components/flash-messages.jsx
@@ -3,17 +3,21 @@
  */
 
 import React from "react";
-import {Alert} from "react-bootstrap";
+import {Alert, Col, Row} from "react-bootstrap";
 
 export default props =>
-    <div>
-        {props.messages.map((message, index) =>
-            <Alert
-                key={index}
-                bsStyle={message.type === 'error' ? 'danger' : message.type}
-                onClick={props.removeMessage(index)}
-            >
-                <p>{message.text}</p>
-            </Alert>
-        )}
+    <div style={{position: "absolute", bottom: 0, zIndex: 99999, width: "100%"}}>
+        <Row>
+            <Col xs={12} sm={8} smOffset={2} md={6} mdOffset={3}>
+                {props.messages.map((message, index) =>
+                    <Alert
+                        key={index}
+                        bsStyle={message.type === 'error' ? 'danger' : message.type}
+                        onClick={props.removeMessage(index)}
+                    >
+                        <p>{message.text}</p>
+                    </Alert>
+                )}
+            </Col>
+        </Row>
     </div>;
